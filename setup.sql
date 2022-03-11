d08ecde26a4d9f5474bb282d565e9a73bc11f304
Kyle D Haywood <kylehaywood@hotmail.com>
2022-03-10 23:02:59 -0500

    first commit

---
+++ setup.sql
@@ -0,0 +1,47 @@
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    hobbie TEXT,
+    active BOOLEAN NOT NULL DEFAULT 1
+);
+
+INSERT INTO user (
+    first_name,
+    last_name,
+    hobbies
+) VALUES (
+    "Kyle",
+    "Haywood",
+    "Board games"
+);
+
+INSERT INTO user (
+    first_name,
+    last_name,
+    hobbies
+) VALUES (
+    "Brett",
+    "Bryant",
+    "Darts"
+);
+
+INSERT INTO user (
+    first_name,
+    last_name,
+    hobbies
+) VALUES (
+    "Ray",
+    "Garcia",
+    "Poker"
+);
+
+INSERT INTO user (
+    first_name,
+    last_name,
+    hobbies
+) VALUES (
+    "Jasmine",
+    "Green-key",
+    "Getting lost"
+);
